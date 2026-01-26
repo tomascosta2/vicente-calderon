fae4390dcca84485276bf67a119ef37b0bd2e753
Tomas Costa <tomascostapp@gmail.com>
2026-01-26 09:58:38 -0300

    det

--- app/page.tsx
+++ app/page.tsx
@@ -34,27 +34,27 @@ export default function Home() {
       <div className="bg-[var(--primary)]/80 size-[600px] rounded-full left-1/2 transform hidden md:block -translate-x-1/2 absolute -z-50  blur-[800px] -top-[400px]"></div>
 
       {/* <img src="/images/logo.png" className="h-[25px] object-contain mx-auto" alt="Logo" /> */}
-      <header className="bg-linear-0 from-[#0E0E0E] to-[#1C1B1B] max-w-[85%] w-[500px] rounded-full mx-auto border border-[var(--primary)]/30 z-50">
+      {/* <header className="bg-linear-0 from-[#0E0E0E] to-[#1C1B1B] max-w-[85%] w-[500px] rounded-full mx-auto border border-[var(--primary)]/30 z-50">
         <div className="cf-container">
           <h3 className="text-center uppercase text-[var(--text-primary)]/80 tracking-widest text-[12px] py-3 leading-[130%]">
             <span>Buscamos setters y closers con experiencia</span>
           </h3>
         </div>
-      </header>
+      </header> */}
 
       {/* Sección VSL (siempre visible) */}
       <section className="mt-6 pb-[60px] md:pb-[100px] relative overflow-clip">
         <div className="cf-container">
-          <h1 className="text-center text-[22px] md:text-[38px] font-bold uppercase leading-[140%] md:px-4">
+          <h1 className="text-center text-[28px] max-w-[600px] mx-auto md:text-[42px] font-bold uppercase leading-[140%] md:px-4">
             <span>
-              Dejá de perseguir leads fríos. Sumate a un sistema con oportunidades reales
+              RECLUTAMIENTO DE CLOSER Y SETTERS.
             </span>
           </h1>
-          {/* <p className="text-[var(--primary)] text-center mt-2">
-            En este video te explico paso a paso cómo funciona el sistema y si realmente es para vos.
-          </p> */}
+          <p className="text-[var(--primary)] text-center mt-2 max-w-[360px] mx-auto">
+            Trabaja como setter o closer en nuestra empresa. No es ningún curso, es una oferta laboral.
+          </p>
           <section className="relative">
-            <div className="bg-[#131313] p-1 pt-0 border-1 border-[var(--primary)] overflow-clip rounded-[12px] md:rounded-[16px] border-[var(--primary)] mt-6 max-w-[900px] mx-auto">
+            <div className="bg-[#131313] p-1 pt-0 border-1 border-[var(--primary)] overflow-clip rounded-[12px] md:rounded-[16px] border-[var(--primary)] mt-6 max-w-[800px] mx-auto">
               <div className="p-2 text-center text-[12px] uppercase text-[var(--text-primary)] tracking-widest bg-[#131313]">
                 Mirá este video completo
               </div>
@@ -75,22 +75,22 @@ export default function Home() {
                 className="cf-btn --sm"
                 style={{ margin: 0 }}
               >
-                ¡POSTULARME PARA SETTER!
+                POSTULACIÓN SETTER
               </button>
               <button
                 className="cf-btn --sm"
                 style={{ margin: 0 }}
               >
-                ¡POSTULARME PARA CLOSER!
+                POSTULACIÓN SETTER
               </button>
             </div>
-            <div className="h-[1px] relative overflow-clip max-w-[212px] mx-auto mt-4">
+            {/* <div className="h-[1px] relative overflow-clip max-w-[212px] mx-auto mt-4">
               <div className="bg-radial from-white to-black/0 size-[200px]"></div>
-            </div>
-            <p className="text-center mt-4 leading-[90%] text-white/40 mx-auto max-w-[350px] text-[14px] flex items-center justify-center gap-2">
+            </div> */}
+            {/* <p className="text-center mt-4 leading-[90%] text-white/40 mx-auto max-w-[350px] text-[14px] flex items-center justify-center gap-2">
               +500 Llamadas Diarias
               <img src="/images/Estrella.svg" alt="Estrellas" />
-            </p>
+            </p> */}
           </div>
         </div>
       </section>
@@ -98,12 +98,12 @@ export default function Home() {
       <div className="bg-[var(--primary)]/80 size-[800px] rounded-full left-[-400px] absolute -z-50  blur-[500px] top-[700px]"></div>
       <div className="bg-[var(--primary)]/80 size-[800px] rounded-full right-[-400px] absolute -z-50  blur-[500px] top-[700px]"></div>
 
-      <section className="py-[100px] relative z-20">
+      <section className="py-[40px] relative z-20">
         <div className="cf-container">
-          <h2 className="text-[36px] md:text-[44px] font-bold text-white text-center max-w-[500px] leading-[130%] mx-auto">
+          {/* <h2 className="text-[36px] md:text-[44px] font-bold text-white text-center max-w-[500px] leading-[130%] mx-auto">
             Un Poco Más Sobre Nosotros...
-          </h2>
-          <div className="my-8 md:my-12 max-w-[900px] mx-auto space-y-6">
+          </h2> */}
+          <div className="mb-8 md:mb-12 max-w-[900px] mx-auto space-y-6 grid md:grid-cols-2 md:gap-8 md:space-y-0">
             {TESTIMONIALS_VIDEO_PAGE.map((testimonial) => {
               return (
                 <div
@@ -120,8 +120,8 @@ export default function Home() {
                       </div>
                       <div className="bg-[var(--primary)] size-[900px] top-[-450px] blur-[150px] opacity-[70%] rounded-full absolute -right-[450px] -z-40"></div>
                       <div className="bg-[var(--primary)] size-[900px] bottom-[-450px] blur-[150px] opacity-[70%] rounded-full absolute -left-[450px] -z-40"></div>
-                      <div className="relative bg-[#171717] z-50 p-4 md:p-[50px] rounded-[20px] flex md:flex-row flex-col gap-4 md:gap-8">
-                        <div className="w-full max-w-[400px] aspect-video rounded-[10px] overflow-hidden">
+                      <div className="relative bg-[#171717] z-50 rounded-[20px] flex md:flex-row flex-col gap-4 md:gap-8">
+                        <div className="w-full aspect-video rounded-[21px] overflow-hidden">
                           <iframe
                             className="w-full h-full border border-white/20 rounded-[10px]"
                             src={testimonial.video}
@@ -129,7 +129,7 @@ export default function Home() {
                             allow="autoplay; fullscreen"
                           ></iframe>
                         </div>
-                        <div className="py-4 flex flex-col justify-between">
+                        {/* <div className="py-4 flex flex-col justify-between">
                           <div>
                             <h3 className="text-[24px] leading-[120%] font-bold">
                               {testimonial.titulo}
@@ -144,7 +144,7 @@ export default function Home() {
                               {testimonial.dato}
                             </p>
                           </div>
-                        </div>
+                        </div> */}
                       </div>
                     </div>
                   </div>
